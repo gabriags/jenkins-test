a9102104ebb094b7d71833dd680a0c7a1d18134a
gabriags <grabri.ags98@gmail.com>
2025-03-25 10:43:12 -0300

    added github url

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,6 +3,11 @@ Jenkinsfile (Declarative Pipeline)
 pipeline {
     agent { docker { image 'python:3.13.2-alpine3.21' } }
     stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/example/repo.git'
+            }
+        }
         stage('build') {
             steps {
                 sh 'python --version'
