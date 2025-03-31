b82c8d67de87c61f82e70dc69f24d010cb03571c
gabriags <grabri.ags98@gmail.com>
2025-03-31 11:20:55 -0300

    Making a Hello world

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,12 @@
 Jenkinsfile (Declarative Pipeline)
 /* Requires the Docker Pipeline plugin */
 pipeline {
-    agent { docker { image 'python:3.13.2-alpine3.21' } }
+    agent any
+
     stages {
-        stage('build') {
+        stage('Hello') {
             steps {
-                sh 'python --version'
+                echo 'hello world'
             }
         }
     }
